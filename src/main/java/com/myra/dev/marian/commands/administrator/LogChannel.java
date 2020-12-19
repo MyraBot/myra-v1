691a621312125a4362ebb6717cdb6ff9c93cd849
Marian <69646823+Marian06@users.noreply.github.com>
2020-12-19 14:07:05 +0100

    v1.1.0 [Changed description of command usage]

--- src/main/java/com/myra/dev/marian/commands/administrator/LogChannel.java
+++ src/main/java/com/myra/dev/marian/commands/administrator/LogChannel.java
@@ -1,4 +1,4 @@
-package com.myra.dev.marian.listeners.logging;
+package com.myra.dev.marian.commands.administrator;
 
 import com.myra.dev.marian.database.allMethods.Database;
 import com.myra.dev.marian.management.commands.Command;
@@ -18,26 +18,23 @@ public class LogChannel implements Command {
 
     @Override
     public void execute(CommandContext ctx) throws Exception {
-        // Get utilities
-        Utilities utilities = Utilities.getUtils();
+        Utilities utilities = Utilities.getUtils(); // Get utilities
         // Usage
         if (ctx.getArguments().length != 1) {
             EmbedBuilder usage = new EmbedBuilder()
                     .setAuthor("log channel", null, ctx.getAuthor().getEffectiveAvatarUrl())
                     .setColor(utilities.gray)
-                    .addField("`" + ctx.getPrefix() + "log channel <channel>`", "\uD83E\uDDFE │ Set the channel where all logging actions should go", false);
+                    .addField("`" + ctx.getPrefix() + "log channel <channel>`", "\uD83E\uDDFE │ Set the channel where logging messages go", false);
             ctx.getChannel().sendMessage(usage.build()).queue();
             return;
         }
-        /**
-         * Change log channel
-         */
-        // Get channel
-        TextChannel channel = utilities.getTextChannel(ctx.getEvent(), ctx.getArguments()[0], "log channel", "\uD83E\uDDFE");
+
+        // Change log channel
+        TextChannel channel = utilities.getTextChannel(ctx.getEvent(), ctx.getArguments()[0], "log channel", "\uD83E\uDDFE"); // Get channel
         if (channel == null) return;
-        // Get database
-        Database db = new Database(ctx.getGuild());
-        // Remove logs channel
+
+        Database db = new Database(ctx.getGuild()); // Get database
+        // Remove log channel
         if (channel.getId().equals(db.getString("logChannel"))) {
             // Update database
             db.set("logChannel", "not set");
@@ -46,20 +43,21 @@ public class LogChannel implements Command {
             return;
         }
         // Change log channel
-        else
+        else {
             // Update database
             db.set("logChannel", channel.getId());
-        // Success message
-        EmbedBuilder logChannel = new EmbedBuilder()
-                .setAuthor("log channel", null, ctx.getAuthor().getEffectiveAvatarUrl())
-                .setColor(utilities.blue)
-                .addField("\uD83E\uDDFE │ Log channel changed", "Log channel changed to **" + channel.getName() + "**", false);
-        ctx.getChannel().sendMessage(logChannel.build()).queue();
-        // Success message in the new log channel
-        EmbedBuilder logChannelInfo = new EmbedBuilder()
-                .setAuthor("log channel", null, ctx.getAuthor().getEffectiveAvatarUrl())
-                .setColor(utilities.blue)
-                .addField("\uD83E\uDDFE │ Log channel changed", "Logging actions are now send in here", false);
-        channel.sendMessage(logChannelInfo.build()).queue();
+            // Success message
+            EmbedBuilder logChannel = new EmbedBuilder()
+                    .setAuthor("log channel", null, ctx.getAuthor().getEffectiveAvatarUrl())
+                    .setColor(utilities.blue)
+                    .addField("\uD83E\uDDFE │ Log channel changed", "Log channel changed to **" + channel.getName() + "**", false);
+            ctx.getChannel().sendMessage(logChannel.build()).queue();
+            // Success message in the new log channel
+            EmbedBuilder logChannelInfo = new EmbedBuilder()
+                    .setAuthor("log channel", null, ctx.getAuthor().getEffectiveAvatarUrl())
+                    .setColor(utilities.blue)
+                    .addField("\uD83E\uDDFE │ Log channel changed", "Logging actions are now send in here", false);
+            channel.sendMessage(logChannelInfo.build()).queue();
+        }
     }
 }
