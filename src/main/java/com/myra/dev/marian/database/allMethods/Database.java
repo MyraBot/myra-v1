362f6488bea8ce8f560f63b969d03c0a811f23ca
Marian <69646823+Marian06@users.noreply.github.com>
2020-12-25 10:47:39 +0100

    v1.1.0 [Added Database.get, Database.setString and Database.set]

--- src/main/java/com/myra/dev/marian/database/allMethods/Database.java
+++ src/main/java/com/myra/dev/marian/database/allMethods/Database.java
@@ -4,7 +4,8 @@ import com.myra.dev.marian.database.MongoDb;
 import net.dv8tion.jda.api.entities.Guild;
 import org.bson.Document;
 
-import static com.mongodb.client.model.Filters.*;
+import java.util.List;
+
 import static com.mongodb.client.model.Filters.eq;
 
 public class Database {
@@ -26,13 +27,37 @@ public class Database {
     public String getString(String key) {
         return mongoDb.getCollection("guilds").find(eq("guildId", guild.getId())).first().getString(key);
     }
+
     //get boolean
     public boolean getBoolean(String key) {
         return mongoDb.getCollection("guilds").find(eq("guildId", guild.getId())).first().getBoolean(key);
     }
 
+    /**
+     * @param key   The key to search.
+     * @param clazz The return type.
+     * @param <T>   The returned class type.
+     * @return Returns the value of the given key as the specified class type.
+     */
+    public <T> T get(String key, Class<T> clazz) {
+        return mongoDb.getCollection("guilds").find(eq("guildId", guild.getId())).first().get(key, clazz);
+    }
+
+    public <T> List<T> getList(String key, Class<T> clazz) {
+        return mongoDb.getCollection("guilds").find(eq("guildId", guild.getId())).first().getList(key, clazz);
+    }
+
     //replace String
-    public void set(String key, String value) {
+    public void setString(String key, String value) {
+        // Replace value
+        Document updatedDocument = mongoDb.getCollection("guilds").find(eq("guildId", guild.getId())).first();
+        updatedDocument.replace(key, value);
+        // Update database
+        mongoDb.getCollection("guilds").findOneAndReplace(mongoDb.getCollection("guilds").find(eq("guildId", guild.getId())).first(), updatedDocument);
+    }
+
+    //replace String
+    public void set(String key, Object value) {
         // Replace value
         Document updatedDocument = mongoDb.getCollection("guilds").find(eq("guildId", guild.getId())).first();
         updatedDocument.replace(key, value);
