a7a95bf82c5b994c59465e4bed2779bd6532cd10
Marian <69646823+Marian06@users.noreply.github.com>
2021-01-01 15:00:35 +0100

    v1.1.1 [Fixed GET Request for DiscordBoats.hasVoted]

--- src/main/java/com/myra/dev/marian/utilities/APIs/DiscordBoats.java
+++ src/main/java/com/myra/dev/marian/utilities/APIs/DiscordBoats.java
@@ -1,10 +1,10 @@
 package com.myra.dev.marian.utilities.APIs;
 
-import com.myra.dev.marian.Bot;
 import com.myra.dev.marian.utilities.Config;
 import com.myra.dev.marian.utilities.Utilities;
 import net.dv8tion.jda.api.entities.User;
 import okhttp3.Request;
+import okhttp3.Response;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -18,11 +18,17 @@ public class DiscordBoats {
 
     public boolean hasVoted(User user) throws IOException {
         // Create request
-        Request request = new Request.Builder()
-                .url("https://discord.boats/api/bot/" + Config.myra + "/voted?id=" + user.getId())
+        final Request request = new Request.Builder()
+                .url("https://discord.boats/api/bot/" + Config.myra + "/voted?id=" + user.getId() + "/")
                 .build();
-        final String response = Utilities.HTTP_CLIENT.newCall(request).execute().body().string(); // Get response
-        if (new JSONObject(response).getBoolean("error")) return false; // User hasn't logged in in discord.boats yet
-        return new JSONObject(response).getBoolean("voted"); // Return voted state
+
+        final Response execute = Utilities.HTTP_CLIENT.newCall(request).execute();
+        final String response = execute.body().string();
+        execute.close();
+
+        final JSONObject json = new JSONObject(response);
+
+        if (json.getBoolean("error")) return false; // User hasn't logged in in discord.boats yet
+        return json.getBoolean("voted"); // Return voted state
     }
 }
