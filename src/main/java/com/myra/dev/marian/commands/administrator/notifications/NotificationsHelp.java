7d8f0bca612bed48deec35a9783408f350184ec4
Marian <69646823+Marian06@users.noreply.github.com>
2021-01-29 14:31:24 +0100

    v1.1.1 [Used CommandUsage class & set notifications as the name and notification as a alias]

--- src/main/java/com/myra/dev/marian/commands/administrator/notifications/NotificationsHelp.java
+++ src/main/java/com/myra/dev/marian/commands/administrator/notifications/NotificationsHelp.java
@@ -4,13 +4,15 @@ package com.myra.dev.marian.commands.administrator.notifications;
 import com.myra.dev.marian.management.commands.Command;
 import com.myra.dev.marian.management.commands.CommandContext;
 import com.myra.dev.marian.management.commands.CommandSubscribe;
+import com.myra.dev.marian.utilities.EmbedMessage.CommandUsage;
+import com.myra.dev.marian.utilities.EmbedMessage.Usage;
 import com.myra.dev.marian.utilities.Permissions;
 import com.myra.dev.marian.utilities.Utilities;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 @CommandSubscribe(
-        name = "notification",
-        aliases = {"notifications"},
+        name = "notifications",
+        aliases = {"notification"},
         requires = Permissions.ADMINISTRATOR
 )
 public class NotificationsHelp implements Command {
@@ -19,13 +21,30 @@ public class NotificationsHelp implements Command {
         // Check for no arguments
         if (ctx.getArguments().length != 0) return;
         // Send message
-        EmbedBuilder notificationUsage = new EmbedBuilder()
-                .setAuthor("notification", null, ctx.getAuthor().getEffectiveAvatarUrl())
-                .setColor(Utilities.getUtils().gray)
-                .addField("`" + ctx.getPrefix() + "notifications twitch <streamer>`", "\uD83D\uDCE1 │ Add and remove auto notifications for a twitch streamer", false)
-                .addField("`" + ctx.getPrefix() + "notifications youtube <youtube channel>`", "\uD83D\uDCFA │ Add and remove auto notifications for YouTube", false)
-                .addField("`" + ctx.getPrefix() + "notifications list`", "\uD83D\uDD14 │ Displays all users you get notification from", false)
-                .addField("`" + ctx.getPrefix() + "notifications channel <channel>`", "\uD83D\uDCC1 │ Set the channel, the notifications will go", false);
-        ctx.getChannel().sendMessage(notificationUsage.build()).queue();
+        new CommandUsage(ctx.getEvent())
+                .setCommand("notifications")
+                .addUsages(
+                        new Usage()
+                                .setUsage("notifications twitch <streamer>")
+                                .setDescription("Add and remove auto notifications for a twitch streamer")
+                                .setEmoji("\uD83D\uDCE1"),
+                        new Usage()
+                                .setUsage("notifications youtube <youtube channel>")
+                                .setDescription("Add and remove auto notifications for YouTube")
+                                .setEmoji("\uD83D\uDCFA"),
+                        new Usage()
+                                .setUsage("notifications list")
+                                .setDescription("Displays all users you get notification from")
+                                .setEmoji("\uD83D\uDD14"),
+                        new Usage()
+                                .setUsage("notifications channel <channel>")
+                                .setDescription("Set the channel, the notifications will go")
+                                .setEmoji("\uD83D\uDCC1"),
+                        new Usage()
+                                .setUsage("notifications message")
+                                .setDescription("Set a notification message, which is send once a notifications is send")
+                                .setEmoji("\uD83D\uDCEF")
+                )
+                .send();
     }
 }
